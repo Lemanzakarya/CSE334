8ae32e53fc32367da5882cc89d831a841d1b050a
ElifKeskinn <elif.keskin233@gmail.com>
2024-03-23 15:54:24 +0300

    solution for errors

--- nova.h
+++ nova.h
@@ -1,3 +1,13 @@
+#ifndef NOVA_H
+#define NOVA_H
+
+typedef struct {
+    int char_type;
+    // Diğer üyeler buraya eklenebilir
+} YYSTYPE;
+
+extern YYSTYPE yylval;
+
 #define LETTER 1    // Harf
 #define DIGIT 2     // Rakam
 #define WHITESPACE 3 // Boşluk karakteri (örneğin, boşluk veya sekme)
@@ -6,3 +16,5 @@
 #define EOL 6
 #define ERROR 7
 #define PRINT 8
+
+#endif /* NOVA_H */
